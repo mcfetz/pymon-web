6e6dc478a59759c8c9d1b2920ee04d05fec62996
Daniel Heise <daniel@familie-heise.de>
2026-07-23 22:56:32 +0200

    fix: strip surrounding quotes from PYMON_API_URL
    Some container runtimes (e.g. Portainer) pass env var values with
    literal quote characters included when the value is entered with
    quotes in the UI. This caused nginx to receive an invalid
    proxy_pass directive.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 : "${PYMON_API_URL:=http://pymon-server:5000}"
 
+# Strip surrounding quotes that some container runtimes pass literally
+PYMON_API_URL=$(echo "$PYMON_API_URL" | sed 's/^["'\'']*//;s/["'\'']*$//')
+
 echo "pymon-web: API backend → ${PYMON_API_URL}"
 
 envsubst '${PYMON_API_URL}' \
